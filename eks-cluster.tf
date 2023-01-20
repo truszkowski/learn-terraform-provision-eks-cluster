db99cb548089f9fac9b4ff6970b2e560aa204989
Jacob Martin <jakub.wit.martin@gmail.com>
2023-01-20 13:51:26 +0100

    Update eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,3 +1,7 @@
+locals {
+  deployment_k8s_rbac_group = "spacelift-worker-deployer"
+}
+
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "17.24.0"
@@ -18,14 +22,7 @@ module "eks" {
       additional_userdata           = "echo foo bar"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
       asg_desired_capacity          = 2
-    },
-    {
-      name                          = "worker-group-2"
-      instance_type                 = "t2.medium"
-      additional_userdata           = "echo foo bar"
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
-      asg_desired_capacity          = 1
-    },
+    }
   ]
     
   map_roles = [
@@ -43,4 +40,86 @@ data "aws_eks_cluster" "cluster" {
 
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_id
+}
+  
+resource "kubernetes_cluster_role" "deployer" {
+  depends_on = [module.eks]
+
+  metadata {
+    name = "spacelift-worker-deployer-clusterrole"
+  }
+
+  rule {
+    api_groups = [""]
+    resources  = ["namespaces", "nodes"]
+    verbs      = ["get", "list", "watch"]
+  }
+}
+  
+resource "kubernetes_cluster_role_binding" "deployer" {
+  depends_on = [module.eks]
+
+  metadata {
+    name = "spacelift-worker-deployer-clusterrole-binding"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "spacelift-worker-deployer-clusterrole"
+  }
+
+  subject {
+    kind      = "Group"
+    name      = local.deployment_k8s_rbac_group
+    api_group = "rbac.authorization.k8s.io"
+  }
+}
+  
+resource "kubernetes_role" "deployer" {
+  depends_on = [module.eks]
+
+  metadata {
+    name      = "spacelift-worker-deployer-role"
+    namespace = "default"
+  }
+
+  rule {
+    api_groups = [""]
+    resources  = ["pods", "secrets", "services", "serviceaccounts"]
+    verbs      = ["create", "get", "list", "update", "delete", "patch", "watch"]
+  }
+
+  rule {
+    api_groups = ["apps"]
+    resources  = ["deployments", "statefulsets"]
+    verbs      = ["create", "get", "list", "update", "delete", "patch", "watch"]
+  }
+
+  rule {
+    api_groups = ["apps"]
+    resources  = ["replicasets"]
+    verbs      = ["get", "list", "watch"]
+  }
+}
+  
+resource "kubernetes_role_binding" "deployer" {
+  depends_on = [module.eks]
+
+  metadata {
+    name      = "spacelift-worker-deployer-role-binding"
+    namespace = "default"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "Role"
+    name      = "spacelift-worker-deployer-role"
+  }
+
+  subject {
+    kind      = "Group"
+    name      = local.deployment_k8s_rbac_group
+    api_group = "rbac.authorization.k8s.io"
+  }
 }
